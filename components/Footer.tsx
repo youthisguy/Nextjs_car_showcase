39369cbb77de771a72cf3aefbc765d11b6e8c434
Sam Steel <samuel.ini145@gmail.com>
2023-09-15 00:10:24 +0100

    Animations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,19 @@
+"use client"
+
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import { footerVariants } from '@utils/motion';
 
 import { footerLinks } from '@/constants/index';
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+    <motion.footer 
+               variants={footerVariants}
+               initial="hidden"
+               whileInView="show"
+               className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
       <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
           <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
@@ -37,7 +45,7 @@ const Footer = () => {
           </Link>
         </div>
       </div>
-    </footer>
+    </motion.footer>
   );
 };
 
--- utils/motion.ts
+++ utils/motion.ts
@@ -109,3 +109,24 @@ export const textVariant2 = {
       opacity: 1
     }
   };
+
+  export const footerVariants = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 140,
+      },
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: 'spring',
+        stiffness: 80,
+        delay: 0.5,
+      },
+    },
+  };
