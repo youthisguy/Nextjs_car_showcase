33c26d1f7ff092b737d5ec6aa7ee53a33978b1a8
Sam Steel <samuel.ini145@gmail.com>
2023-09-15 09:53:30 +0100

    Bug Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -112,7 +112,6 @@ export default function Home() {
           !loading && (
           <div className='home__error-container'>
             <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-            <p>{allCars?.message}</p>
           </div>
           )
         )}
